ff3877a703995ff14209e389bfada9b4e4b8c668
Mohamed Amine MEBARKI <mohamed.mebarki@UNODIS.DZ>
2025-04-13 22:38:03 +0100

    refreshing the core syntaxe

---
+++ JavaScriptReminder/play.js
@@ -0,0 +1,9 @@
+let age = 30;
+let name = 'Amine';
+let hasHobbies = false;
+
+function describePerson(age,name,hasHobbies) {
+console.log('My name is ' + name + ' , my age is ' + age + ` , and i ${hasHobbies? 'have': 'dont have'} hobbies`);
+}
+
+describePerson(age,name,hasHobbies);
