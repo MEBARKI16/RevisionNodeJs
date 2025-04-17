a5170adabb0428f998bfca9fea49fd5ec9f2094c
Mohamed Amine MEBARKI <mohamed.mebarki@UNODIS.DZ>
2025-04-17 10:10:41 +0100

    diffrence bettween let and var

--- JavaScriptReminder/play.js
+++ JavaScriptReminder/play.js
@@ -1,3 +1,4 @@
+/*
 let age = 30;
 let name = 'Amine';
 let hasHobbies = false;
@@ -6,4 +7,15 @@ function describePerson(age,name,hasHobbies) {
 console.log('My name is ' + name + ' , my age is ' + age + ` , and i ${hasHobbies? 'have': 'dont have'} hobbies`);
 }
 
-describePerson(age,name,hasHobbies);
+describePerson(age,name,hasHobbies); **/
+
+function testVarLet() {
+        var x = 4;
+        var x = 14;
+        let y = 5;
+        let y = 15;
+
+    console.log(x);
+    console.log(y);
+}
+testVarLet();
